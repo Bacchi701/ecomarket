4d0400b002d7200545487c7a62654c83feb46a92
Bacchi701 <br.bacchi@duocuc.cl>
2025-05-29 01:25:34 -0400

    commit

---
+++ src/main/java/com/ecomarket/ecomarket/model/Cliente.java
@@ -0,0 +1,34 @@
+package com.ecomarket.ecomarket.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "cliente")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique=true, length = 13, nullable=false)
+    private String run;
+
+    @Column(nullable=false)
+    private String nombre;
+
+    @Column(nullable=false)
+    private String apellido;
+
+    @Column(nullable=false)
+    private String correo;
+
+    @Column(nullable=false)
+    private String direccionEnvio;
+    
+}
