68c1f2dd059b77cf3ed5060d52bb87b659d85f71
Bacchi701 <br.bacchi@duocuc.cl>
2025-05-29 01:28:49 -0400

    commit

---
+++ src/main/java/com/ecomarket/ecomarket/controller/ClienteController.java
@@ -0,0 +1,31 @@
+package com.ecomarket.ecomarket.controller;
+
+import com.ecomarket.ecomarket.model.Cliente;
+import com.ecomarket.ecomarket.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listar(){
+        List<Cliente> clientes = clienteService.findAll();
+        if (clientes.isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(clientes);
+    }
+    
+    
+}
